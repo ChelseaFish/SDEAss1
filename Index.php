fdcadf4983cb1212e044dc47ae294759cd4e09c9
Chelsea Couche <superfishyface@hotmail.com>
2017-10-07 16:56:52 +1100

    v1.1

---
+++ Index.php
@@ -0,0 +1,32 @@
+
+<html>
+<head>
+<title> Bob's Auto Parts</title>
+</head>
+
+<body>
+<form  action="welcome.php" method="post">
+FirstName: <input type="text" name="firstname" value="<?php echo $fname;?>"><br/><br/>
+LastName:  <input type="text" name="lastname" value="<?php echo $lname;?>"><br/><br/>
+Number of Tyres: <input type="number" name="tyres" value="<?php echo $tyre;?>"><br/><br/>
+<input type="submit" name="Calculate" ><br/>
+    
+    <?php 
+	{
+		try {
+			$conn = new PDO("sqlsrv:server = tcp:sdeass1.database.windows.net,1433; Database = SDEAss1Database", "SDEAss1", "SDEM+CAss1");
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		catch (PDOException $e) {
+			print("Error connecting to SQL Server.");
+			die(print_r($e));
+		}
+		print "Connected successfully";
+		
+		$conn->close();
+	}
+    ?>
+	
+</form>
+</body>
+</html>
--- Welcome.php
+++ Welcome.php
@@ -5,10 +5,34 @@
 </head>
 <body>
     <?php
+	
+		$firstname = $_POST["firstname"];
+		$lastname = $_POST["lastname"];
+		$tyres = $_POST["tyres"];
+		$amount = $tyres * 110;
 
-    echo $_POST["firstname"]." ".$_POST["lastname"].'<br/>';
-    echo "total amount due is: ".($_POST["tyres"]* 110).'<br/>';
+		echo $firstname." ".$lastname.'<br/>';
+		echo "total amount due is: ".$amount.'<br/>';
 
-    ?>
+		try {
+			$conn = new PDO("sqlsrv:server = tcp:sdeass1.database.windows.net,1433; Database = SDEAss1Database", "SDEAss1", "SDEM+CAss1");
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		catch (PDOException $e) {
+			print("Error connecting to SQL Server.");
+			die(print_r($e));
+		}
+
+		$sql = "INSERT INTO Orders (firstname, lastname, noOftyres, amount) VALUES ('$firstname', '$lastname', '$tyres', $amount)";
+
+		if ($conn->query($sql) == TRUE) {
+		echo "New record created successfully";
+		} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+
+		$conn->close();
+	
+	?>
 </body>
 </html>
