e39b404917b98743ac58035df28d4f201a102ca2
unknown <9991662@AD224-002.ds.swin.edu.au>
2017-10-15 14:33:58 +1100

    testing a change

--- Index.php
+++ Index.php
@@ -8,7 +8,7 @@
 <form  action="Welcome.php" method="post">
 FirstName: <input type="text" name="firstname" value="<?php echo $fname;?>"><br/><br/>
 LastName:  <input type="text" name="lastname" value="<?php echo $lname;?>"><br/><br/>
-Number of Tyres: <input type="number" name="tyres" value="<?php echo $tyre;?>"><br/><br/>
+Number of Tyres SHREK WAS HERE: <input type="number" name="tyres" value="<?php echo $tyre;?>"><br/><br/>
 <input type="submit" name="Calculate" ><br/>
     
     <?php 
