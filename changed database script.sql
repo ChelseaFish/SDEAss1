9601b483d9d54d76c75fac5d8f838136898bcb6f
unknown <9991662@AD224-002.ds.swin.edu.au>
2017-10-15 12:44:27 +1100

    Capitalisation change for vagrant deployment + the sql scripts

---
+++ changed database script.sql
@@ -0,0 +1,10 @@
+ALTER TABLE Orders 
+ADD amount NUMERIC NULL;
+
+GO
+
+UPDATE Orders
+SET Amount=noOftyres*110
+WHERE Amount IS NULL;
+
+GO
---
+++ createtable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Orders (
+firstname VARCHAR(50) NOT NULL PRIMARY KEY,
+lastname VARCHAR(50) NOT NULL,
+noOftyres INT NOT NULL);
+
+GO
